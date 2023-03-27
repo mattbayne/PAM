738213f92f184a108497c919a2889439e6f1399a
nlespera <nlespera@us.ibm.com>
2023-03-27 18:43:06 -0400

    made auth/nonauth nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {NavLink} from 'react-router-dom';
+import {AuthContext} from "../firebase/Auth";
+import SignOutButton from "./SignOut";
 import '../App.css'
 
-function Navigation() {
+const Navigation = ()=>{
+    const {currentUser} = useContext(AuthContext);
+    return <div>{currentUser ? <NavigationAuth /> : <NavigationNoAuth /> }</div>
+}
+
+const NavigationAuth = ()=>{
+    return (
+        <nav className='navigation'>
+            <ul>
+                <li>
+                    <NavLink to='/'>Landing</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/private'>Private</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/changepassword'>Change Password</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+const NavigationNoAuth = ()=> {
     return (
         <nav className='navigation'>
             <ul>
@@ -14,12 +39,6 @@ function Navigation() {
                 </li>
                 <li>
                     <NavLink to='/signin'>Sign-In</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/private'>Private</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/changepassword'>Change Password</NavLink>
                 </li>
             </ul>
         </nav>
