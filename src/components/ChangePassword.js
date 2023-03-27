247b64ec31747b024156ebc2b9c0ae9800177499
nlespera <nlespera@us.ibm.com>
2023-03-27 13:02:56 -0400

    compiles now, no functionality implemented

---
+++ src/components/ChangePassword.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignOutButton from "./SignOut";
+import '../App.css'
+
+function ChangePassword() {
+    return (
+        <div>
+            <h2>Welcome to PAM</h2>
+        </div>
+    );
+}
+
+export default ChangePassword;
---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+import '../App.css'
+import React from 'react';
+
+function SignUp() {
+    return (
+        <div>
+            <h2>Sign up page</h2>
+        </div>
+    );
+}
+
+export default SignUp;
