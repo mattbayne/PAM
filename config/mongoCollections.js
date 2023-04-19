5b90c4ae0a2382003f5b2cd3befdb090f2f5363f
mattbayne <44953288+mattbayne@users.noreply.github.com>
2023-04-19 19:13:45 -0400

    added mongodb config files

---
+++ config/mongoCollections.js
@@ -0,0 +1,20 @@
+const dbConnection = require('./mongoConnection');
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection.dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+
+//not sure what our collections will be yet
+module.exports = {
+  data: getCollectionFn('data'),
+};
