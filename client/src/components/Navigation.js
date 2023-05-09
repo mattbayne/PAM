7984fa704a09d9d8dd21748c8e7a794c23edef42
nlespera <nlespera@us.ibm.com>
2023-05-09 19:20:12 -0400

    reverting file to previous version because nav was broke

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -8,23 +8,24 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import logo from '../img/logo.jpeg'
-import {useContext, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {AuthContext} from "../firebase/Auth";
 import { NavLink } from 'react-router-dom';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import axios from "axios";
 import noUser from '../img/noUser.webp'
-import { useNavigate } from 'react-router-dom';
-import DashboardContext from "./Dashboard/DashboardContext";
 
 
+const pages = ['Products', 'Pricing', 'Blog'];
 
 function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = useState(null);
     const [anchorElUser, setAnchorElUser] = useState(null);
-    const { setSelectedFunctionality } = useContext(DashboardContext);
-
     // const [avatar, setAvatar] = useState(null)
 
     const { currentUser, avatar } = useContext(AuthContext);
@@ -32,8 +33,15 @@ function ResponsiveAppBar() {
         ? ["Account", "Dashboard", "Logout"]
         : ["Login", "Signup"];
 
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
     const handleOpenUserMenu = (event) => {
         setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
     };
 
     const handleCloseUserMenu = () => {
@@ -45,7 +53,9 @@ function ResponsiveAppBar() {
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <Box sx={{ flexGrow: 0, width: '150px', height: '150px', display: { xs: 'none', md: 'flex' }, mr: 1 }}>
-                        <img src={logo} alt='logo' style={{ maxWidth: '100%', maxHeight: '100%', cursor: 'pointer' }} draggable={"false"} onClick={() => setSelectedFunctionality('home')} />
+                        <NavLink to="/">
+                            <img src={logo} alt='logo' style={{ maxWidth: '100%', maxHeight: '100%' }} draggable="false" />
+                        </NavLink>
                     </Box>
                     <Typography
                         variant="h1"
@@ -64,11 +74,14 @@ function ResponsiveAppBar() {
                     >
                         Personalized Assistant & Manager
                     </Typography>
-                        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, cursor: 'pointer' }} onClick={() => setSelectedFunctionality('home')} />
+                    <NavLink to='/' style={{color: 'inherit'}}>
+                        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    </NavLink>
                     <Typography
                         variant="h5"
                         noWrap
                         component="a"
+                        href="/"
                         sx={{
                             mr: 2,
                             display: { xs: 'flex', md: 'none' },
@@ -78,9 +91,7 @@ function ResponsiveAppBar() {
                             letterSpacing: '.3rem',
                             color: 'inherit',
                             textDecoration: 'none',
-                            cursor: 'pointer'
                         }}
-                        onClick={() => setSelectedFunctionality('home')}
                     >
                         PAM
                     </Typography>
@@ -95,11 +106,11 @@ function ResponsiveAppBar() {
                                 </IconButton>
                             ) : (
                                 <IconButton onClick={handleOpenUserMenu}
-                                    size="large"
-                                    edge="start"
-                                    color="inherit"
-                                    aria-label="menu"
-                                    sx={{ mr: 2, p: 0 }}
+                                            size="large"
+                                            edge="start"
+                                            color="inherit"
+                                            aria-label="menu"
+                                            sx={{ mr: 2, p: 0 }}
                                 >
                                     <MenuIcon sx={{fontSize: '2.5rem'}} />
                                 </IconButton>
@@ -146,7 +157,7 @@ function ResponsiveAppBar() {
                                         </NavLink>
                                     )}
                                     {setting === "Dashboard" && (
-                                        <NavLink onClick={() => setSelectedFunctionality('home')} style={{color: '#076efd'}} >
+                                        <NavLink to="/" style={{color: '#076efd'}} >
                                             <Typography textAlign="center">{setting}</Typography>
                                         </NavLink>
                                     )}
